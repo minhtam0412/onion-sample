e45249707e1eec409e307c3ec470fa71dbe11ea4
Tam Ngo <minhtam0412@gmail.com>
2022-01-07 10:35:05 +0700

    update code

--- OnionArchitecture/Persistence/DependencyInjection.cs
+++ OnionArchitecture/Persistence/DependencyInjection.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Application.Interfaces;
+﻿using Application.Interfaces;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
